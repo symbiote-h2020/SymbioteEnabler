323c4d992ead0025d23acdc8828e374e8441de1b
Mario Kušek <mario.kusek@fer.hr>
2018-10-16 12:30:36 +0200

    Create download_latest_images.sh

---
+++ resources/docker/usb/download_latest_images.sh
@@ -0,0 +1,13 @@
+docker pull rabbitmq:3-alpine
+docker pull mongo:3.6
+docker pull symbioteh2020/symbiote-enablerconfig
+docker pull symbioteh2020/symbiote-aam
+docker pull symbioteh2020/symbiote-eureka
+docker pull symbioteh2020/symbiote-rap
+docker pull symbioteh2020/symbiote-rh
+docker pull symbioteh2020/symbiote-monitoring
+docker pull symbioteh2020/symbiote-erm
+docker pull symbioteh2020/symbiote-epp
+docker pull symbioteh2020/symbiote-enablerlogicexample
+docker pull symbioteh2020/symbiote-rappluginexample
+docker pull symbioteh2020/symbiote-nginx
